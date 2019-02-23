586669ec28ace435ea800c6ae01bb25751a96c49
inglec <inglec@tcd.ie>
2019-02-23 20:46:43 +0000

    Convert array of single boolean to boolean

--- src/child.js
+++ src/child.js
@@ -7,16 +7,20 @@ const { ERROR, SUCCESS } = require('./message_types');
 
 const readFileAsync = promisify(readFile);
 
-const createVM = (builtin = false, external = false) => (
-  new NodeVM({
-    console: 'off',
-    require: {
-      builtin,
-      external,
-      context: 'sandbox',
-    },
-  })
-);
+const convertArg = (arg) => {
+  if (typeof arg === 'boolean') {
+    return arg;
+  }
+
+  if (Array.isArray(arg)) {
+    return arg.length === 1 && typeof arg[0] === 'boolean' ? arg[0] : arg;
+  }
+
+  process.stderr(`expected type "boolean" or "array", but received "${typeof arg}"`);
+  process.exit(1);
+
+  return null;
+};
 
 const send = (type, body) => process.send({ type, body });
 
@@ -27,12 +31,14 @@ function main() {
       name: 'builtin',
       alias: 'b',
       type: String,
+      defaultValue: false,
       multiple: true,
     },
     {
       name: 'external',
       alias: 'e',
       type: String,
+      defaultValue: false,
       multiple: true,
     },
   ]);
@@ -42,7 +48,18 @@ function main() {
     process.exit(1);
   }
 
-  const vm = createVM(args.builtin, args.external);
+  // Convert possible [ Boolean ] to Boolean.
+  const builtin = convertArg(args.builtin);
+  const external = convertArg(args.external);
+
+  const vm = new NodeVM({
+    console: 'off',
+    require: {
+      builtin,
+      external,
+      context: 'sandbox',
+    },
+  });
 
   readFileAsync(args.path)
     .then((file) => {
